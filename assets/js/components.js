9b852f0ce5944eb4cb1f36863fd5897f4818366a
countrytherapy <itcountrytherapy@gmail.com>
2026-06-25 07:16:30 +0000

    NDIS

--- assets/js/components.js
+++ assets/js/components.js
@@ -29,6 +29,7 @@
       <a href="tel:+610494758039">0494 758 039</a>
       <a href="mailto:reception@countrytherapy.com.au">reception@countrytherapy.com.au</a>
     </div>
+    <span class="tb-ndis">Works with NDIS participants</span>
   </div>
 </div>
 <nav id="nav" aria-label="Main navigation">
@@ -167,6 +168,10 @@
         <span class="ft-colour-dot" style="background:#E85D7A"></span>
         <span class="ft-colour-dot" style="background:#6B8F71"></span>
       </div>
+<<<<<<< HEAD
+=======
+      <span class="ft-ndis">Works with NDIS participants</span>
+>>>>>>> 35d448c (NDIS)
     </div>
   </div>
 </footer>
